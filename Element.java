c0b39893d15ee3e193c125082cdca3bb1bbf8504
Rachael Cheung <47546717+RachaelCheung@users.noreply.github.com>
2019-03-12 22:16:23 -0400

    initial non-working element class for get method

---
+++ Element.java
@@ -0,0 +1,19 @@
+public class Element {
+	private int    intElement;
+    private double doubleElement;
+    private String stringElement;
+	private int type;
+	
+	public Element(int pos, int[] intElements, 
+							double[] doubleElements,
+							String[] stringElements,
+							int[] elementTypes) {
+								
+		intElement = intElements[pos];
+		doubleElement = doubleElements[pos];
+		stringElement = stringElements[pos];
+		type = elementTypes[pos];	
+	}
+	
+	
+}
