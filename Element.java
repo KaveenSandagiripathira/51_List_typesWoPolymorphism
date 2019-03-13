1f7130e49c9b6cdb206af0b2cb337c0012ec36d3
Kaveen Sandagiripathira <ksandagiripathir@stuy.edu>
2019-03-13 13:51:41 -0400

    Kyle and Amy's Solution

--- Element.java
+++ Element.java
@@ -1,20 +1,52 @@
+public class Element{
+    // ================================================================
+    // FIELDS
+    private int type;
+    private int intValue;
+    private double doubleValue;
+    private String stringValue;
+    
+    public static final int INTS = 0;
+    public static final int DOUBLES = 1;
+    public static final int STRINGS = 2;
 
-public class Element {
-	private int    intElement;
-    private double doubleElement;
-    private String stringElement;
-	private int type;
-	
-	public Element(int pos, int[] intElements, 
-							double[] doubleElements,
-							String[] stringElements,
-							int[] elementTypes) {
-								
-		intElement = intElements[pos];
-		doubleElement = doubleElements[pos];
-		stringElement = stringElements[pos];
-		type = elementTypes[pos];	
-	}
-	
+    
+    // ================================================================
+    // CONSTRUCTOR
+    public Element(int valType, int intVal, double doubleVal, String stringVal){
+        type = valType;
+        intValue = intVal;
+        doubleValue = doubleVal;
+        stringValue = stringVal;
+    }
+
+    // ================================================================
+    // METHODS
+	// public int getType(){
+		// return type;
+	// }
+
+	// public int getInt(){
+		// return intValue;
+	// }
+
+	// public double getDouble(){
+		// return doubleValue;
+	// }
+
+	// public String getString(){
+		// return stringValue;
+	// }
+
+    public String toString(){
+        String stringRep = "";
+        if      (type == INTS)    stringRep += intValue;
+        else if (type == DOUBLES) stringRep += doubleValue;
+        else                      stringRep += stringValue;
+        return stringRep;
+    }
+
 
 }
+
+
--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -6,11 +6,11 @@
 
 public class List_inArraySlots {
 
-    private int[]    intElements;
-    private double[] doubleElements;
-    private String[] stringElements;
-    private int filledElements; // the number of elements in this list
+    // ================================================================
+    // FIELDS
     
+    private int filledElements; // the number of elements in this list
+
     /* type identifier for each element
        That is, typeOfElements[i] == 0 means element i is an integer;
                                      1 means element i is a double;
@@ -18,29 +18,28 @@ public class List_inArraySlots {
         Optional extra education in programming (not comp sci):
             replace these "magic numbers" with an "enumerated type".
      */
-    private int[] typeOfElements;
+    private Element[] list;
 
     private static final int INITIAL_CAPACITY = 10;
-	private static final int INT_NUM = 0;
-	private static final int DOUBLE_NUM = 1;
-	private static final int STRING_NUM = 2;
-
+    
+    // ================================================================
+    // CONSTRUCTORS
+    
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-		intElements = new int[INITIAL_CAPACITY];
-		doubleElements = new double[INITIAL_CAPACITY];
-		stringElements = new String[INITIAL_CAPACITY];
-		typeOfElements = new int[INITIAL_CAPACITY];
+      list = new Element[INITIAL_CAPACITY];
     }
-
-
+    
+    // ================================================================
+    // METHODS
+    
     /**
       @return the number of elements in this list
      */
     public int size() {
-		return filledElements;
+      return filledElements;
     }
 
 
@@ -49,16 +48,13 @@ public class List_inArraySlots {
        in [a,b,c,] format
       */
     public String toString() {
-		String output = "[";
-		for (int pos = 0; pos < filledElements; pos++){
-			if (typeOfElements[pos] == INT_NUM)
-				output += intElements[pos] + ", ";
-			else if (typeOfElements[pos] == DOUBLE_NUM)
-				output += doubleElements[pos] + ", ";
-			else if (typeOfElements[pos] == STRING_NUM)
-				output += stringElements[pos] + ", ";
-		}
-		return output += "]";
+      String stringRep = "[";
+      
+      for (int index = 0; index < filledElements; index++)
+        stringRep += list[index] + ",";
+
+      stringRep += "]";
+      return stringRep;
     }
 
 
@@ -72,22 +68,21 @@ public class List_inArraySlots {
                        , double doubleValue
                        , String stringValue
                        ) {
-		if( filledElements == intElements.length) expand();
-		if (type == INT_NUM){
-			intElements[ filledElements] = intValue;
-			typeOfElements[filledElements] = INT_NUM;
-		}
-		else if (type == DOUBLE_NUM){
-			doubleElements[filledElements] = doubleValue;
-			typeOfElements[filledElements] = DOUBLE_NUM;
-		}
-		else if (type == STRING_NUM){
-			stringElements[filledElements] = stringValue;
-			typeOfElements[filledElements] = STRING_NUM;
-		}
-		filledElements++;
-		return true;
+        // expand when at max
+        if (filledElements == list.length) expand();
+        
+        // create new instance using param and add 
+        list[filledElements] = new Element(type, intValue, doubleValue, stringValue);
+
+        // update size
+        filledElements++;
+
+        return true;
      }
+
+    public Element get(int index){
+        return list[index];
+    }
 
 
     /**
@@ -95,38 +90,25 @@ public class List_inArraySlots {
       preserving existing data.
      */
      private void expand() {
-		int[] intBigger = new int[ intElements.length * 2];
-		double[] doubleBigger = new double[ doubleElements.length * 2];
-		String[] stringBigger = new String[ stringElements.length * 2];
-		int[] typeBigger = new int[ typeOfElements.length * 2];
-		
-        for( int elemIndex = 0; elemIndex < filledElements; elemIndex++){
-            intBigger[ elemIndex] = intElements[ elemIndex];
-			doubleBigger[ elemIndex] = doubleElements[ elemIndex];
-			stringBigger[ elemIndex] = stringElements[ elemIndex];
-			typeBigger[ elemIndex] = typeOfElements[ elemIndex];
-		}
-        intElements = intBigger;
-		doubleElements = doubleBigger;
-		stringElements = stringBigger;
-		typeOfElements = typeBigger;
+        System.out.println( "expand... (for debugging)");
+           /* S.O.P. rules for debugging:
+              Working methods should be silent. But during
+              development, the programmer must verify that
+              this method is called when that is appropriate.
+              So test using the println(), then comment it out.
+              */
+
+       //System.out.println( "old length:" + typeOfElements.length);
+
+        // create new list, copy values over, and replace list
+        Element[] bigger = new Element[list.length * 2];
+        for(int index=0;index<list.length; index++){
+            bigger[index] = list[index];
+        }
+        list = bigger;
+
+
+       // System.out.println( "new length:" + typeOfElements.length);
+
      }
-	 
-	 
-	 /**
-      accessor
-      @return element @index from this list
-      precondition: @index is within the bounds of the array.
-          (Having warned the user about this precondition,
-           you should NOT complicate your code to check
-           whether user violated the condition.)
-     */
-    public Element get( int index ) {
-		Element value = new Element(index, intElements, 
-										doubleElements, 
-										stringElements, 
-										typeOfElements);
-		
-		return value;
-    }
 }
